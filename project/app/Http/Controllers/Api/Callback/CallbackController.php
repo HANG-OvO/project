fefa036199c60efd4a7b7316a73f41068795ccea
lh_orz <lh_orz@163.com>
2021-03-29 11:10:04 +0800

    callback

--- project/app/Http/Controllers/Api/Callback/CallbackController.php
+++ project/app/Http/Controllers/Api/Callback/CallbackController.php
@@ -29,44 +29,61 @@ class CallbackController extends Controller
     //注册
     public function taskback(Request $request){
 
-        $params['task_id'] = $request->get('param1'); // 任务Id
-        $params['user_token'] = $request->get('param2'); // 用户标识
-        $params['state'] = $request->get('param3'); // 作答结果
-        $params['time'] = $request->get('time'); // 时间戳
-        $params['sign'] = $request->get('sign'); // 签名
+        try {
 
-        // 验证任务id
-        $task = DB::table('task') -> where('task_id', $params['task_id']) -> get();
-        if (!$task) {
-            // 任务有误记录日志
-            Log::debug("任务未找到",$params);
+            $params['task_id'] = $request->get('param1'); // 任务Id
+            $params['user_token'] = $request->get('param2'); // 用户标识
+            $params['state'] = $request->get('param3'); // 作答结果
+            $params['time'] = $request->get('time'); // 时间戳
+            $params['sign'] = $request->get('sign'); // 签名
+
+            Log::debug("接口参数",$params);
+
+            if (!$params['task_id'] || !$params['user_token']) {
+                // 参数错误记录日志
+                Log::debug("参数错误",$params);
+                return $this->resposne(200);
+            }
+
+            // 验证任务id
+            $task = DB::table('task') -> where('task_id', $params['task_id']) -> get();
+            if (!$task) {
+                // 任务有误记录日志
+                Log::debug("任务未找到",$params);
+                return $this->resposne(200);
+            }
+            // 验证用户
+            $user_info = explode('-',$encrypted = Crypt::decryptString($params['user_token']));
+            $user_id = $user_info[0];
+            $ip = $user_info[3];
+            $user = DB::table('user') -> where('user_id', $user_id) -> get();
+            if (!$user) {
+                // 用户有误，记录日志
+                Log::debug("用户未找到",[$params,$user_info]);
+                return $this->resposne(200);
+            }
+            // 修改任务状态
+            $user_task_log = DB::table('user_task_log')
+                ->where('user_id', $user_id)
+                ->where('task_id', $params['task_id'])
+                ->where('ip', $ip)
+                ->get();
+            if (!$user_task_log) {
+                // 任务未查到记录日志
+                Log::debug("任务记录未找到",[$params,$user_info]);
+                return $this->resposne(200);
+            }
+            $result = DB::update('update user_task_log set state = ? where user_id = ? and task_id = ? and ip = ?', [$params['state'],$user_id, $params['task_id'], $ip]);
+            // 记录执行日志
+            Log::debug("执行结果",[$result]);
             return $this->resposne(200);
-        }
-        // 验证用户
-        $user_info = explode('-',$encrypted = Crypt::decryptString($params['user_token']));
-        $user_id = $user_info[0];
-        $ip = $user_info[3];
-        $user = DB::table('user') -> where('user_id', $user_id) -> get();
-        if (!$user) {
-            // 用户有误，记录日志
-            Log::debug("用户未找到",[$params,$user_info]);
+
+        } catch (\Exception $e) {
+
+            Log::debug("执行结果",[$e->getMessage()]);
             return $this->resposne(200);
+
         }
-        // 修改任务状态
-        $user_task_log = DB::table('user_task_log')
-                            ->where('user_id', $user_id)
-                            ->where('task_id', $params['task_id'])
-                            ->where('ip', $ip)
-                            ->get();
-        if (!$user_task_log) {
-            // 任务未查到记录日志
-            Log::debug("任务记录未找到",[$params,$user_info]);
-            return $this->resposne(200);
-        }
-        $result = DB::update('update user_task_log set state = ? where user_id = ? and task_id = ? and ip = ?', [$params['state'],$user_id, $params['task_id'], $ip]);
-        // 记录执行日志
-        Log::debug("执行结果",[$result]);
-        return $this->resposne(200);
 
     }
 
